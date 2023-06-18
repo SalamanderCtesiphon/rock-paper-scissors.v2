101a029c54594a2eaf1d6bcfdc3b6cea1d7d2d04
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-17 20:40:07 -0500

    implemented a function to determine the computers choice for the game

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('hi')
+
+
+const getComputerChoice = function() {
+  const index = Math.floor(Math.random() * 3);
+  const computerChoices = [ 'Rock', 'Paper', 'Scissors'];
+  return computerChoices[index];
+};
+
+let playerWins = 0;
+let computerwins = 0;
+
+console.log(getComputerChoice());
