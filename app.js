27d1d343ac0f04daeb49a0a42ae7a557b8bf5981
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-18 09:50:02 -0500

    added all the logic and console logs need to play  the game and to check to make sure that everything is working as intended

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-/* let playerSelction = prompt("Choose your item: rock, paper or scissors"); */
-let playerSelction = 'rock';
+let playerSelction = prompt("Choose your item: rock, paper or scissors");
+
 
 const getComputerChoice = function() {
   const index = Math.floor(Math.random() * 3);
@@ -13,6 +13,7 @@ let ties = 0;
 
 
 const playRound = function(playerSelction, computerSelection) {
+  console.log(`player: ${playerSelction} computer: ${computerSelection}`)
   if(playerSelction === 'rock' && computerSelection === 'scissors') {
     return playerWins += 1;
   } else if(playerSelction === 'paper' && computerSelection === 'rock') {
@@ -38,10 +39,21 @@ const game = function() {
   for(let i = 0; i < 5; i++) {
     const computerSelection = getComputerChoice();
     playRound(playerSelction, computerSelection);
+    console.log(`Round ${i + 1}
+computer wins: ${computerWins} 
+player wins: ${playerWins}`)
   };
+  console.log('Final Results:')
   console.log(`ties: ${ties}`);
   console.log(`computer wins: ${computerWins}`);
   console.log(`player wins: ${playerWins}`);
+  if(computerWins > playerWins) {
+    console.log(`computer wins`);
+  } else if(playerWins > computerWins) {
+    console.log('player wins');
+  } else {
+    console.log('tie');
+  }
 };
 
 game();
